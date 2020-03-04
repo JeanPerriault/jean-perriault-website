6e834348972472da8643d7236a417fd71fe70572
Jean Perriault <jpweb.taf@gmail.com>
2020-03-04 22:04:33 +0100

    label files and i18n plugin and middleware

---
+++ plugins/i18n.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
+Vue.use(VueI18n);
+
+export default ({ app, store }) => {
+  // Set i18n instance on app
+  // This way we can use it in middleware and pages asyncData/fetch
+  app.i18n = new VueI18n({
+    locale: store.state.locale,
+    fallbackLocale: 'fr',
+    messages: {
+      fr: Object.assign(
+        require('~/locales/fr.json')
+      ),
+      en: Object.assign(
+        require('~/locales/en.json')
+      ),
+      es: Object.assign(
+        require('~/locales/es.json')
+      )
+    }
+  });
+
+  app.i18n.path = link => {
+    if (app.i18n.locale === app.i18n.fallbackLocale) {
+      return `/${link}`;
+    }
+
+    return `/${app.i18n.locale}/${link}`;
+  };
+};
